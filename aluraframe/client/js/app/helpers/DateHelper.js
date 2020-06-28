08858fb187560db598bd7c36115bef638fd06f78
Conrado Moura <conrado.moura@git.iacit.com.br>
2020-06-28 05:35:28 -0300

    aula4 - Lidar com data é trabalhoso? Chame um ajudante! - template strings and regexfail fast

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,19 @@
 class DateHelper {
 
-    dataParaTexto(data){
-        return data.getDate()
-        + '/' + (data.getMonth() + 1)
-        + '/' + data.getFullYear();
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static dataParaTexto(data){
+        //nao esquecer que eh craze no inicio. e nao aspas simples
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
     
-    textoParaData(texto){
+    static textoParaData(texto){
+        // \d - digit = D - nao numerico
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))  //fail fast
+            throw new Error("Formato deve ser em aaaa-mm-dd");
+
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
